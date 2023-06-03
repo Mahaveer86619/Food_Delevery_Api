7f13c710fb4c2cf519abe30b0518f9e7a01e8e58
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-06-03 18:59:19 +0530

    Initializing Repository

---
+++ src/main/java/com/example/FuddoApi/FuddoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.FuddoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuddoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuddoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FuddoApi/FuddoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FuddoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuddoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
